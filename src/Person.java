45644945afc72daf6dc6f182b61945a727646154
Gaurav Rajput <90279818+gaurav8299@users.noreply.github.com>
2024-06-12 15:55:19 +0530

    Practice Java

---
+++ src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    int age = 20;
+
+    public static void main(String[] args) {
+        Person Ritvik = new Person(); //Object Created
+        System.out.println(Ritvik.age);
+    }
+}
